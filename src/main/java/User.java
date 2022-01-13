90ca66dc56bbaae446633c28a0c3afde30b88d9d
CrudeCod <mikaelcg2@gmail.com>
2022-01-13 10:52:53 +0100

    Fixed the broken package

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,12 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.omazoncli;
 
 import java.io.*;
-
 public class User implements Serializable{
     //Basic info
     private String Username;
